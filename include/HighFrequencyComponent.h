58840fdaa8f9a97cbd8e7973b327f524bc57b033
JanelynAnderson <andersonjanelyn@gmail.com>
2025-12-11 19:18:28 -0600

    Added High Frequency Component Class

---
+++ include/HighFrequencyComponent.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//Local
+#include "include/Component.h"
+
+//Std
+#include <vector>
+namespace dlib {
+    class HighFrequencyComponent : public dlib::Component
+    {
+    public:
+        static std::vector<HighFrequencyComponent> AllCreatedHighFrequencyComponents;
+
+        virtual void HighFrequencyPreStep() = 0;
+        virtual void HighFrequencyPostStep() = 0;
+    };
+};
