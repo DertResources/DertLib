c63fb3f49ac86575bf3896a017e2695345a60c90
JanelynAnderson <andersonjanelyn@gmail.com>
2026-01-22 11:23:53 -0600

    Getting rid of warnings

--- cpp/CANBus.cpp
+++ cpp/CANBus.cpp
@@ -1,13 +1,13 @@
     #include "../include/CANBus.h"
     
     dlib::CANBus::CANBus(std::string _IN_CANBusName)
-    : CTRE_CanBus{std::make_shared<ctre::phoenix6::CANBus>(_IN_CANBusName)}
-    , CANBusName{_IN_CANBusName}
+    : CANBusName{std::move(_IN_CANBusName)}
+    , CTRE_CanBus{std::make_shared<ctre::phoenix6::CANBus>(_IN_CANBusName)}
     {}
     
     dlib::CANBus::CANBus(const dlib::CANBus &obj)
-    : CTRE_CanBus{obj.CTRE_CanBus}
-    , CANBusName{obj.CANBusName}
+    : CANBusName{obj.CANBusName}
+    , CTRE_CanBus{obj.CTRE_CanBus}
     {}
 
     dlib::CANBus::operator ctre::phoenix6::CANBus&() const
